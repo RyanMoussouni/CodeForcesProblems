9e2ac7b4d8ef4a345d0ec27dc53eafef020f76a3
Ryan Moussouni <moussouni.ryan0@gmail.com>
2024-01-25 11:48:32 +0100

    Add Solution.java

---
+++ 1807D/Solution.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedReader input;
+    static StringTokenizer _stk;
+
+    static String readln() throws IOException {
+        String l = input.readLine();
+        if (l != null)
+            _stk = new StringTokenizer(l, " ");
+        return l;
+    }
+
+    static String next() {
+        return _stk.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static void dbg(Object... o) {
+        System.out.println(Arrays.deepToString(o));
+    }
+
+    static PrintWriter output = new PrintWriter(new BufferedWriter(
+	new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        input = new BufferedReader(new InputStreamReader(System.in));
+
+	readln();
+	var nLines = nextInt();
+
+	readln();
+	var n = nextInt();
+	var q = nextInt();
+
+	for (int i = 0; i<nLines; i++) {
+	    var prefixSum = computePrefixSum(n);
+
+	    for (int j=0; j<q; j++) {
+		    readln();
+		    var l = nextInt();
+		    var r = nextInt();
+		    var k = nextInt();
+
+		    var csn = prefixSum.get(n-1);
+		    var csl = prefixSum.get(l);
+		    var csr = prefixSum.get(r); 
+
+		    var isOdd = csn - csr + csl + k*(r-l);
+
+		    System.out.println(isOdd);
+	    }
+	}
+    }
+
+    private static List<Integer> computePrefixSum(Integer n) {
+	    readln();
+
+	    var arr = new ArrayList<Integer>();
+	    var crt = 0;
+	    for (int i=0; i<n; i++) {
+		    crt += nextInt();
+		    arr.add(crt);
+	    }
+
+	    return arr;
+    }
+}
