51c01463ce4291c79ccf92563de230482851c272
Ryan Moussouni <moussouni.ryan0@gmail.com>
2024-02-21 16:26:22 +0100

    Add 1324A

---
+++ 1610A/Solution.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedReader input;
+    static StringTokenizer _stk;
+
+    static String readln() throws IOException {
+        String l = input.readLine();
+        if (l != null)
+            _stk = new StringTokenizer(l, " ");
+        return l;
+    }
+
+    static String next() {
+        return _stk.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static void dbg(Object... o) {
+        System.out.println(Arrays.deepToString(o));
+    }
+
+    static PrintWriter output = new PrintWriter(new BufferedWriter(
+	new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        input = new BufferedReader(new InputStreamReader(System.in));
+
+	readln();
+	var nLines = nextInt();
+
+	for (int i=0; i<nLines; i++) {
+		readln();
+		var n = nextInt();
+		var m = nextInt();
+
+		if (n==1 && m==1) {
+			System.out.println(0);
+		} 
+		if (n!=1 && m==1 || n==1 && m!=1) {
+			System.out.println(1);
+		}
+		if (n!=1 && m!=1) {
+			System.out.println(2);
+		}
+	}
+    }
+}
