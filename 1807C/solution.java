f483bc2aa041686a473197bdd5f6af7ec3030df4
Ryan Moussouni <moussouni.ryan0@gmail.com>
2024-02-21 16:28:05 +0100

    Add 1807C

---
+++ 1807C/solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedReader input;
+    static StringTokenizer _stk;
+
+    static String readln() throws IOException {
+        String l = input.readLine();
+        if (l != null)
+            _stk = new StringTokenizer(l, " ");
+        return l;
+    }
+
+    static String next() {
+        return _stk.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static void dbg(Object... o) {
+        System.out.println(Arrays.deepToString(o));
+    }
+
+    static PrintWriter output = new PrintWriter(new BufferedWriter(
+	new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        input = new BufferedReader(new InputStreamReader(System.in));
+
+	readln();
+
+	var nLines = nextInt();
+
+	for (int i=0; i<nLines; i++) {
+		readln();
+		nextInt();
+		var line = readln();
+		var hashTable = toHashTable(line);
+	}
+    }
+
+    private static HashMap<String, String> toHashTable(String line) {
+	    HashMap<String, String> hashMap = new HashMap<>();
+
+	    
+    }
+}
