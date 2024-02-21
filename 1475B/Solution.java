72250b9be050814e12b8597faf6ae1c1c4eee0bc
Ryan Moussouni <moussouni.ryan0@gmail.com>
2024-02-21 14:52:45 +0100

    Add 1475B

---
+++ 1475B/Solution.java
@@ -0,0 +1,54 @@
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedReader input;
+    static StringTokenizer _stk;
+
+    static String readln() throws IOException {
+        String l = input.readLine();
+        if (l != null)
+            _stk = new StringTokenizer(l, " ");
+        return l;
+    }
+
+    static String next() {
+        return _stk.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static void dbg(Object... o) {
+        System.out.println(Arrays.deepToString(o));
+    }
+
+    static PrintWriter output = new PrintWriter(new BufferedWriter(
+	new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        input = new BufferedReader(new InputStreamReader(System.in));
+
+	readln();
+	var nLines = nextInt();
+
+	for (int i=0; i<nLines; i++) {
+		readln();
+		var n = nextInt();
+
+		var y = n % 2020;
+
+		var isPossible = ((n-y*2021) % 2020) == 0;
+
+		if (isPossible) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
+    }
+}
